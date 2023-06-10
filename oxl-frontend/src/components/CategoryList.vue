a173b06474106713e9b67e2cf8275da4d5d61186
KuKa <69753899+jkbkupczyk@users.noreply.github.com>
2023-06-10 23:23:24 +0200

    Revert "Add favourite products, show/hide password on registration form or lo…"

--- oxl-frontend/src/components/CategoryList.vue
+++ oxl-frontend/src/components/CategoryList.vue
@@ -1,50 +1,54 @@
 <template>
-  <div class="w-100">
-    <h5 class="pt-4">Wybierz kategorię</h5>
-    <ul class="list-group h-100">
-      <li :class="{'selected-category': (cat.name == selectedCategory)}" class="list-group-item custom-list-item"
-          v-for="cat in categories" v-bind:key="cat.dbaseId" v-on:click="selectCategory(cat.name)">{{ cat.name }}
-      </li>
-    </ul>
-  </div>
+    <div class="w-100">
+        <h5 class="pt-4" >Wybierz kategorię</h5>
+        <ul class="list-group h-100" >
+            <li :class="{'selected-category': (cat.name == selectedCategory)}" class="list-group-item custom-list-item" v-for="cat in categories" v-bind:key="cat.dbaseId" v-on:click="selectCategory(cat.name)">{{ cat.name }}</li>
+        </ul>
+    </div>
 
 </template>
 
 <script lang="ts">
-import {defineComponent, PropType} from 'vue';
-import {Category} from '@/data/entities';
+import { defineComponent } from 'vue';
+import { HttpHandler } from '@/data/httpHandler';
+import { mapGetters, mapState } from 'vuex';
+import { useStore } from 'vuex';
+import { Category } from '@/data/entities';
+import { PropType } from 'vue';
 
 export default defineComponent({
-  name: "CategoryList",
-  props: {
-    categories: {
-      type: Object as PropType<Category[]>,
-      required: true
+    name:"CategoryList",
+    props:{
+        categories: {
+            type: Object as PropType<Category[]>,
+            required: true
+        },
+        selectedCategory:{
+            type: Object as PropType<string>,
+            required: true
+        }
     },
-    selectedCategory: {
-      type: Object as PropType<string>,
-      required: true
+    methods:{
+       selectCategory(cName: string){
+            this.$emit("selectCategory", cName);
+       },
     }
-  },
-  methods: {
-    selectCategory(cName: string) {
-      this.$emit("selectCategory", cName);
-    },
-  }
 })
 
 </script>
 
 <style>
-.custom-list-item {
-  cursor: pointer;
+
+.custom-list-item{
+    cursor: pointer;
 }
 
-.custom-list-item:hover {
-  background-color: gold;
+.custom-list-item:hover{
+    background-color: gold;
 }
 
-.selected-category {
-  background-color: aqua !important;
+.selected-category{
+    background-color: aqua !important;
 }
+
 </style>
--- oxl-frontend/src/data/httpHandler.ts
+++ oxl-frontend/src/data/httpHandler.ts
@@ -1,17 +1,5 @@
 //import { Axios } from "axios";
-import {
-    Product,
-    NewProductForm,
-    User,
-    Category,
-    LoginResponse,
-    RegistrationForm,
-    RegisterResponse,
-    DefaultErrorResponse,
-    AddProductResponse,
-    Alert,
-    AlertTypes,
-} from "./entities";
+import {Product, NewProductForm, User, Category, LoginResponse, RegistrationForm, RegisterResponse, DefaultErrorResponse, AddProductResponse, Alert, AlertTypes} from "./entities";
 import  StoreState  from "@/store/index"; //TODO: Sprawnic, czy to nie jest zdrutowane
 import { useStore } from 'vuex';
 import { Context } from "./context";
@@ -34,11 +22,11 @@ function urlBuilder(x: string): string {
 }
 
 function headerBuilder(){
-    const context: Context = StoreState.getters.context; //TODO: To dotyczy tego wyzej i tez moze byc zdrutowane ale dziala wiec ye*ac
+    var context: Context = StoreState.getters.context; //TODO: To dotyczy tego wyzej i tez moze byc zdrutowane ale dziala wiec ye*ac
     if(!context.currentJWT){
         console.log("JWT token not provided");
     }
-    const header = {
+    var header = {
         Authorization: `Baerer ${context.currentJWT}`,
         'Content-Type': `application/json`, 
     };
@@ -48,7 +36,6 @@ function headerBuilder(){
 
 const urls = {
     products: urlBuilder("products"),
-    favProducts: urlBuilder("favourites"),
     orders: `${protocol}://${hostname}:${port}/orders`,
 
     login: urlBuilder("login"),
@@ -66,14 +53,11 @@ export class HttpHandler{
         return axios.get(urls.products,{headers: headerBuilder() }).then((response: { data: Product[]; }) => response.data).catch((error: any) => console.log(error.response));
     }
 
-    loadFavouriteProducts(): Promise<Product[]> {
-        return axios.get(urls.favProducts, {headers: headerBuilder() }).then((response: { data: Product[]; }) => response.data).catch((error: any) => console.log(error.response));
-    }
-
     loadStoredProduct(): Promise<Product[]>{
-        const id = useStore().state.actualProductId;
+        var id = useStore().state.actualProductId;
         return axios.get(`${urls.products}/${id}`, {headers: headerBuilder() }).then((response: { data: Product[]; }) => response.data).catch((error: any) => console.log(error.response));
     }
+
 
     loadCategories() : Promise<Category[]>{
         return axios.get(urls.categories).then((response: {data: Category[]}) => response.data).catch((error: DefaultErrorResponse) => console.log(error));
@@ -83,9 +67,10 @@ export class HttpHandler{
         return axios.get(urls.users).then((response: {data: User[]}) => response.data).catch(() => console.log());
     }
 
+    
     login(loginData : any): Promise<LoginResponse>{
         console.log(loginData.email);
-        const tmp = JSON.stringify({
+        var tmp = JSON.stringify({
             "email": loginData.email,
             "password": secureLogin(loginData.password)
         });
@@ -96,7 +81,7 @@ export class HttpHandler{
     }
 
     register(registerData: RegistrationForm): Promise<RegisterResponse | DefaultErrorResponse>{
-        const tmp = JSON.stringify({
+        var tmp = JSON.stringify({
             "name": registerData.firstName,
             "surname": registerData.lastName,
             "username": (`${registerData.firstName}_${registerData.lastName}_${Math.floor(Math.random()*99999)}`).toLowerCase(),
@@ -115,7 +100,7 @@ export class HttpHandler{
     }
 
     addProduct(product: NewProductForm): Promise<AddProductResponse | DefaultErrorResponse>{
-        const tmp = JSON.stringify({
+        var tmp = JSON.stringify({
             name: product.name,
 
         });
